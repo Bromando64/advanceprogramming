0a09741a15fd8a26b46f652ac469d326f653643f
Nishant Parajuli <nishant.parajuli123@gmail.com>
2023-05-01 19:31:00 +0545

    created controller for DBconnectiona and Constants and fixed runtime

---
+++ Coursework/src/main/java/resources/MyConstants.java
@@ -0,0 +1,13 @@
+package resources;
+
+public class MyConstants {
+	//Database Configuration
+	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/coursework";
+	public static final String DB_USER_NAME = "root";
+	public static final String DB_USER_PASSWORD = "";
+	
+	//Query Config
+	
+
+}
